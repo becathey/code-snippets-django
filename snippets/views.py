911902223b127102cab3be0a6e59f23d803d3b1e
Bill Cathey <billcathey@hotmail.com>
2023-02-21 16:43:18 -0500

    Add permission_classes attribute to SnippetList and SnippetDetail view classes

--- snippets/views.py
+++ snippets/views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.models import User
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Snippet
 from .serializers import SnippetSerializer, UserSerializer
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -13,6 +14,7 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly)
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
