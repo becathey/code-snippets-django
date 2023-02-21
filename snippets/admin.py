f2cdc9956356a360deb9b88fd38ff8b295e66fa1
Bill Cathey <billcathey@hotmail.com>
2023-02-21 15:33:55 -0500

    Set highlighted as a read-only field

--- snippets/admin.py
+++ snippets/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Snippet
 
-admin.site.register(Snippet)
+class SnippetAdmin(admin.ModelAdmin):
+    readonly_fields = ('highlighted',)
+admin.site.register(Snippet, SnippetAdmin)
