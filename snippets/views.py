b62f2b2aa1a06c29ecb75bc623a6be32776c366e
Bill Cathey <billcathey@hotmail.com>
2023-02-22 13:13:09 -0500

    Add highlighted snippet endpoint

--- snippets/views.py
+++ snippets/views.py
@@ -1,11 +1,20 @@
 from django.contrib.auth.models import User
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, renderers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
+
 from .models import Snippet
 from .permissions import IsOwnerOrReadOnly
 from .serializers import SnippetSerializer, UserSerializer
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = (renderers.StaticHTMLRenderer,)
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 @api_view(["GET"])
 def api_root(request, format=None):
