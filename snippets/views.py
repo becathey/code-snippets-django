98ce8e5bc68da262b22c428e38375ac419149462
Bill Cathey <billcathey@hotmail.com>
2023-02-21 19:08:35 -0500

    Implement object-level custom permission

--- snippets/views.py
+++ snippets/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import generics, permissions
 from .models import Snippet
+from .permissions import IsOwnerOrReadOnly
 from .serializers import SnippetSerializer, UserSerializer
 
 class SnippetList(generics.ListCreateAPIView):
@@ -14,7 +15,10 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly,
+    )
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
