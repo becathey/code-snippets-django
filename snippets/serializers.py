20e776cb064417934bd23cf0420ce24aa0b97247
Bill Cathey <billcathey@hotmail.com>
2023-02-22 17:22:55 -0500

    Implement hyperlinked relationships between entities

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -2,13 +2,16 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name="snippet-highlight", format="html")
 
     class Meta:
         model = Snippet
         fields = (
+            "url",
             "id",
+            "highlight",
             "title",
             "code",
             "linenos",
@@ -17,9 +20,9 @@ class SnippetSerializer(serializers.ModelSerializer):
             "owner",
         )
 
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name="snippet-detail", read_only=True)
 
     class Meta:
         model = User
-        fields = ("id", "username", "snippets")
+        fields = ("url", "id", "username", "snippets")
